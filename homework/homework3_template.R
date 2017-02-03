30d77a48d2e636f76047028acbe149c3da36a4ec
Damian Thomas <damian.r.thomas@gmail.com>
2017-02-03 16:54:45 -0500

    Adding homework 3 template

---
+++ homework/homework3_template.R
@@ -0,0 +1,42 @@
+#-------------------------------------------------------------------------------
+# HOMEWORK 3
+#
+#
+# DESCRIPTION  --------
+#
+# In this assignment we will use internet resources to solidify our existing
+# knowledge and prepare ourselves for the most exciting and challenging part of
+# the course. The goals are to become more comfortable with the R syntax we 
+# have encountered so far, and to deepen our understanding of how R works so we
+# can shift our focus to higher level analysis tasks.
+#
+#
+# INSTRUCTIONS --------
+#
+#  Go to the main url for the course:
+#      https://www.datacamp.com/courses/free-introduction-to-r
+#
+#  Click on "Start Course For Free"
+#
+#  Create a user account or log in (free)
+#
+#  For full credit, complete the following chapters:
+#      (1) Intro to basics
+#      (2) Vectors
+#      (5) Data Frames
+#
+# For extra credit, complete additional chapters of your choice.
+#
+# TO SUBMIT --------
+#
+# In your browser, go back to the main url for the course (above). Your progress
+# (% completed) in each chapter should be clearly visible. Print the page to a
+# PDF file and submit it in the hw3 folder. Make sure you only share it with
+# instructors or just with Damian. Additional instructions will be posted to
+# the course page on piazza
+#-------------------------------------------------------------------------------
+
+# The following R command should open the link browser. Good luck!
+
+browseURL("https://www.datacamp.com/courses/free-introduction-to-r")
+
